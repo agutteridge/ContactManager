4b73e53b8466f1d476ebc8d75357e3dc3ac7bb86
agutteridge <alicegutteridge@gmail.com>
2014-02-09 23:00:23 +0000

    added JUnit test, changed getContacts(String), added prettyprint(), changed HashSet to LinkedHashSet

--- ContactManagerImpl.java
+++ ContactManagerImpl.java
@@ -2,64 +2,101 @@ import java.util.Calendar;
 import java.util.List;
 import java.util.Set;
 import java.util.*;
+import org.junit.*;
+import static org.junit.Assert.*;
 
 public class ContactManagerImpl {
-	private HashSet<Contact> contactSet = null;
+	private LinkedHashSet<Contact> contactSet = null;
 	private FutureMeeting futureList = null; //sorted list?
 
-	public void addNewContact(String name, String notes){
-		if (contactSet == null){
-			contactSet = new HashSet<Contact>();
-		}
-		if (name == null || notes == null){
-			throw new NullPointerException();
-		}
-		Contact anotherContact = new ContactImpl(name);
-		anotherContact.addNotes(notes);
-		contactSet.add(anotherContact);
+	@Test
+	public void testAddNewContact(){
+		ContactManagerImpl database = new ContactManagerImpl();
+		database.addNewContact("sam", "he is someone");
+		database.addNewContact("sam", "");
+		String result = database.prettyPrint();
+		System.out.println("CONTACT SET: " + result);
+		assertEquals(result, "sam, 0, he is someone; sam, 0, ; "); //iterator does not return in same order!!!
 	}
 
-	public Set<Contact> getContacts(int... ids){ //allows arguments with any number of ints (including zero)
-		if (ids.length == 0) { //if getContacts() is called
-	    	throw new IllegalArgumentException("No IDs given.");
-	    }
-
-	    Contact[] contactArray = contactSet.toArray(new Contact[contactSet.size()]); //turn into array so that elements can be returned
-		Set<Contact> result = new HashSet<Contact>(); 
-
-		for (int i = 0; i < ids.length; i++) { //iterating through args
-			System.out.println("ARG BEING TESTED: " + ids[i]);
-		    for (int n = 0; n < contactArray.length; n++) { //iterating through contactSet
-		    	if (ids[i] == (contactArray[i].getId())) {
-		    		result.add(contactArray[i]);
-		    		System.out.println("found " + contactArray[i].getName());
-		    	}
-		    }	
+	public void addNewContact(String name, String notes) throws NullPointerException {
+		if (contactSet == null){
+			contactSet = new LinkedHashSet<Contact>();
 		}
 
-		if (result.isEmpty()){
-	    	throw new IllegalArgumentException("No matching IDs found.");			
-		}		
-		return result;
-	} 
+		int newId = 0;
+		// while (getContacts(newId)){
+		// 	newId++;
+		// }
 
-	public Set<Contact> getContacts(String name){ //Repeating myself
+		try {
+			if (name.equals(null) || notes.equals(null)){
+				throw new NullPointerException();
+			}
+			Contact anotherContact = new ContactImpl(name, newId);
+			anotherContact.addNotes(notes);
+			contactSet.add(anotherContact);
+		} catch (NullPointerException e) {
+			System.out.println("name or notes are null");
+			e.printStackTrace();
+		}
+	}
+
+	// public Set<Contact> getContacts(int... ids){ //allows arguments with any number of ints (including zero)
+	//     Contact[] contactArray = contactSet.toArray(new Contact[contactSet.size()]); //turn into array so that elements can be returned
+	// 	Set<Contact> result = new LinkedHashSet<Contact>();
+
+	// 	for (int i = 0; i < ids.length; i++) { //iterating through args
+	// 		System.out.println("ARG BEING TESTED: " + ids[i]);
+	// 	    for (int n = 0; n < contactArray.length; n++) { //iterating through contactSet
+	// 	    	if (ids[i] == (contactArray[i].getId())) {
+	// 	    		result.add(contactArray[i]);
+	// 	    		System.out.println("found " + contactArray[i].getName());
+	// 	    	}
+	// 	    }	
+	// 	}
+
+	// 	if (result.isEmpty()){
+	//     	throw new IllegalArgumentException("No matching IDs found.");			
+	// 	}		
+	// 	return result;
+	// } 
+
+	public Set<Contact> getContacts(String name){
 		if (name == null) { 
-	    	throw new NullPointerException();
+	    	throw new NullPointerException(); //catch
 	    }
+		
+		Iterator<Contact> iterator = contactSet.iterator();
+		Set<Contact> result = new LinkedHashSet<Contact>();
+		
+		while (iterator.hasNext()){
+			Contact person = iterator.next();
+			if (person.getName().contains(name)){ //boolean test for whether name of Contact object contains name
+				result.add(person); //how does it increment?
+			}
+		}
 
-	    Contact[] contactArray = contactSet.toArray(new Contact[0]); //turn into array so that elements can be returned
-		Set<Contact> result = new HashSet<Contact>(); 
+		try {
+			if (result.isEmpty()){
+		    	throw new IllegalArgumentException();			
+			}		
+		} catch (IllegalArgumentException e){
+			System.out.println("No match found.");
+			e.printStackTrace();
+		}
 
-	    for (int n = 0; n < contactArray.length; n++) { //iterating through contactSet
-	    	if (name.equals(contactArray[n].getName())) { //currently does not compare substrings!!!!!!!!!!
-	    		result.add(contactArray[n]);
-	    	}
-	    }	
+		return result;
+	}
 
-		if (result.isEmpty()){
-	    	throw new IllegalArgumentException("No matching names found.");			
-		}		
+	private String prettyPrint(){
+		Iterator<Contact> iterator = contactSet.iterator();
+		String result = "";
+		while (iterator.hasNext()){
+			Contact person = iterator.next();	
+			result += person.getName() + ", " + person.getId() + ", " + person.getNotes() + "; ";
+		}
+
 		return result;
 	}
 }
