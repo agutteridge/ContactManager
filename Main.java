043b80e1921479cd8333e79ac4c0014e36f16ff4
agutteridge <alicegutteridge@gmail.com>
2014-02-09 23:04:42 +0000

    removed methods from Main, as testing is now done using JUnit

--- Main.java
+++ Main.java
@@ -2,27 +2,7 @@ import java.util.Scanner;
 
 public class Main {
 	
-	public void contactImplTester(){
-		ContactImpl alice = new ContactImpl("Alice");
-		System.out.println(alice.getName());
-		System.out.println(alice.getId());
-		alice.addNotes("something something");
-		alice.addNotes("and again");
-		System.out.println(alice.getNotes());
-
-		System.out.println("");
-		System.out.println("Launching ContactManagerImpl tester...");
-		contactManagerImplTester();
-	}
-
-	public void contactManagerImplTester(){
-		ContactManagerImpl database = new ContactManagerImpl();
-		database.addNewContact("sam", "he is someone");
-		database.addNewContact("turtle", "he is a turtle");
-		database.addNewContact("ghost", "transparent & friendly");
-		database.getContacts(0,1,2,3,4,5,6,7,8,9,10);
-		database.getContacts("sam");		
-	}
+	//testing now moved to JUnit
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner (System.in);
